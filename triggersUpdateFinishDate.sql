86b0e20568cbe1a1118ed6763c123abae4248669
Arocha10 <arocha.p43@gmail.com>
2019-12-08 00:33:17 -0400

    cambio de nombre del archivo para que todos se llamen triggers

---
+++ triggersUpdateFinishDate.sql
@@ -0,0 +1,33 @@
+-- Información sacada de: https://e-mc2.net/es/disparadores-triggers-en-postgresql
+CREATE OR REPLACE FUNCTION updateFinishDate() RETURNS TRIGGER AS $updateFinishDate$
+  DECLARE 
+    temprow RECORD;
+    nuevaFecha Timestamp;
+    fechaLimite Timestamp;
+    fechaBid Timestamp;
+  BEGIN
+    IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE' ) THEN
+      FOR temprow IN
+        SELECT * 
+        FROM Subasta AS s
+        WHERE s.id = NEW.subasta
+          LOOP
+            timeToEnd = now()::timestamp - subasta.fecha_fin;
+
+              IF (timeToEnd <= subasta.fecha_fin) THEN
+                UPDATE Subasta
+                SET fecha_fin = fecha_fin + fecha_limite
+                WHERE Subasta.id = temprow.id;
+              END IF
+          END LOOP;
+    END IF;
+    RETURN NEW;
+  END;
+$updateFinishDate$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS updateFinishDate 
+ON Bid CASCADE;
+
+CREATE TRIGGER updateFinishDate BEFORE INSERT OR UPDATE
+  ON Bid FOR EACH ROW
+  EXECUTE PROCEDURE updateFinishDate();
